c88086d444adab04beb662470d6407bf5c518bfc
Chiagozie Uchenna <princedavisy111@gmail.com>
2025-05-14 05:23:22 +0100

    fixed deployment error

--- src/app/(home)/components/ComingSoon.tsx
+++ src/app/(home)/components/ComingSoon.tsx
@@ -10,15 +10,11 @@ const ComingSoonSection = () => {
   const { t } = useTranslation("home");
 
   const [email, setEmail] = useState("");
-
-  const [status, setStatus] = useState("");
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setStatus("");
 
     try {
       const response = await fetch("/api/newsletter", {
@@ -26,51 +22,53 @@ const ComingSoonSection = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          email,
-        }),
+        body: JSON.stringify({ email }),
       });
+
       console.log(response);
 
       if (response.status === 200) {
         toast.success("Thank you for subscribing!", {
-  style: {
-    color: "#333",
-  },
-});
-
-         setStatus("success");
+          style: { color: "#333" },
+        });
         setEmail("");
       } else {
-
-        toast.error("Something went wrong!", { style: {
-    color: "#333",
-  },});
-        setErrorMessage("Something went wrong!");
-        setStatus("error");
+        toast.error("Something went wrong!", {
+          style: { color: "#333" },
+        });
       }
     } catch (error: any) {
       console.error("Subscription error:", error);
-      setErrorMessage(error?.response?.data?.message);
-      // toast.error(error?.response?.data?.message);
-      setStatus("error");
+      toast.error(error?.response?.data?.message || "Unexpected error occurred.", {
+        style: { color: "#333" },
+      });
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div
-    // className={`w-full ${isAboutUs ? "bg-cta-gradient" : "bg-white"} py-20`}
-    >
+    <div>
       <div className="mx-auto">
         <div className="relative bg-primary-gradient p-2 md:p-8 md:p-12">
           <div className="relative mx-auto max-w-7xl">
             <div className="img1 md:absolute top-10 left-0 mb-2 md:mb-0">
-              <Image src="/images/comingsoon.png" width={1432} height={759} className="w-[200px] md:w-[300px]" alt="Img1" />
+              <Image
+                src="/images/comingsoon.png"
+                width={1432}
+                height={759}
+                className="w-[200px] md:w-[300px]"
+                alt="Img1"
+              />
             </div>
             <div className="img2 absolute top-10 right-0 hidden md:block">
-              <Image src={"/images/comingsoon2.png"} height={1090} width={1505} className="w-[250px]" alt="Img1" />
+              <Image
+                src={"/images/comingsoon2.png"}
+                height={1090}
+                width={1505}
+                className="w-[250px]"
+                alt="Img1"
+              />
             </div>
           </div>
           <div className="max-w-6xl mx-auto flex md:gap-10">
@@ -79,15 +77,16 @@ const ComingSoonSection = () => {
                 <h2 className="text-[25px] md:text-[28px] text-white tracking-wide leading-[35px] md:pt-12 text-center">
                   {renderText(t("coming-soon.heading"))}
                 </h2>
-
-                {/* <p className="text-white/90 text-base md:text-lg">
-                  {renderText(t("comming-soon.subheading"))}
-                </p> */}
               </div>
 
               <div className="w-full justify-center flex flex-col-reverse gap-6 lg:gap-10 items-center">
                 <div className="phone-apps">
-                  <Image src="/images/phoneAppsScreenshot.png" width={1211} height={731} alt="phone-apps-screenshots" />
+                  <Image
+                    src="/images/phoneAppsScreenshot.png"
+                    width={1211}
+                    height={731}
+                    alt="phone-apps-screenshots"
+                  />
                 </div>
 
                 <form
@@ -110,31 +109,8 @@ const ComingSoonSection = () => {
                     {loading ? renderText(t("Loading...")) : renderText(t("join"))}
                   </button>
                 </form>
-              
               </div>
             </div>
-
-            {/* <div className="md:w-72 hidden md:flex flex-col gap-6 pt-10">
-              <img
-                src={RightArrow}
-                alt="Decorative arrow"
-                className="w-40 opacity-60 animate-float-curved animate-draw-attention"
-                style={{
-                  transformOrigin: "center",
-                }}
-              />
-              <div className="flex  transition-all duration-700 ease-out hover:scale-105  items-center gap-2 w-auto ml-20 font-semibold text-blue-custom-700 bg-white px-4 py-3 rounded-full">
-                <span className="">{renderText(t("check"))}</span>
-                <a
-                  href="https://kreatoors.ai"
-                  className="underline transition-colors"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Kreatoors.ai
-                </a>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
