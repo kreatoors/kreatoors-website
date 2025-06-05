7f293d058f709ba2eae11fff3f17fabf295f87b8
Chiagozie Uchenna <princedavisy111@gmail.com>
2025-06-05 18:18:55 +0100

    Revert "Merge pull request #16 from kreatoors/section-updates"
    This reverts commit 88d4a6187d63cd1fa5a3510c11d7930be9ab5fd6, reversing
    changes made to 528673018d24bb54551aa2b11589db76e5689d65.
    
    :wq
    :wq
    :wq

--- src/app/(home)/components/ComingSoon.tsx
+++ src/app/(home)/components/ComingSoon.tsx
@@ -12,12 +12,8 @@ const ComingSoonSection = () => {
 
 
   return (
-
-    <div
-    // className={`w-full ${isAboutUs ? "bg-cta-gradient" : "bg-white"} py-20`}
-    >
-      <div className="mx-auto mt-20">
-
+    <div>
+      <div className="mx-auto">
         <div className="relative bg-primary-gradient p-2 md:p-8 md:p-12">
           <div className="relative mx-auto max-w-7xl">
             <div className="img1 md:absolute top-10 left-0 mb-2 md:mb-0">
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <ComingSoonSection />
+      <ValueStats />
       <BrandSolutionsLanding isPage={false} />
-       <ValueStats />
+      <ComingSoonSection />
       <EGCSection />
       <Stats />
       <AudienceSection />
