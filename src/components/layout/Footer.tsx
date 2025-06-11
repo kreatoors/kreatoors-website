09b34238d0bd5dc1713a78bb2c4d82501d203cc5
auraqule <auraqule@gmail.com>
2025-06-11 10:28:36 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
           {/* Logo and Social Links */}
           <div className="flex flex-row md:flex-col items-center md:items-start justify-between md:space-y-8">
-            <Link scroll={true} href="/" className="text-2xl font-bold text-[#FF8FAB]">
+            <div className="text-2xl font-bold text-[#FF8FAB]">
               {/* <img
                 src="/images/small-logo.png"
                 className="block md:hidden h-16"
@@ -30,11 +30,21 @@ const Footer = () => {
               <div className="h-auto w-auto items-end cursor-pointer flex">
                 <Logo />
               </div>
-            </Link>
+            </div>
             <div className="flex space-x-4">
-              <a href="https://www.linkedin.com/company/kreatoors/posts/?feedView=all" target="_blank" aria-label="LinkedIn">
+              <a
+                href="https://www.linkedin.com/company/kreatoors/posts/?feedView=all"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
                 <div className="w-16 h-16 md:w-12 md:h-12 shadow-lg rounded-full bg-white flex items-center justify-center">
-                  <Image width={25} height={25} src="/images/linkedIn.svg" className="flex items-center justify-center" alt="LinkedIn" />
+                  <Image
+                    width={25}
+                    height={25}
+                    src="/images/linkedIn.svg"
+                    className="flex items-center justify-center"
+                    alt="LinkedIn"
+                  />
                 </div>
               </a>
 
@@ -63,10 +73,15 @@ const Footer = () => {
           {/* Navigation Links */}
           <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-20">
             <div>
-              <h3 className="font-semibold text-black mb-4">{t("H.heading1")}</h3>
+              <h3 className="font-semibold text-black mb-4">
+                {t("H.heading1")}
+              </h3>
               <ul className="grid grid-cols-3 md:block md:space-y-3">
                 <li>
-                  <Link href={"/platform/#features"} className="cursor-pointer text-customGray hover:text-gray-700">
+                  <Link
+                    href={"/platform/#features"}
+                    className="cursor-pointer text-customGray hover:text-gray-700"
+                  >
                     {t("H.title1.point2")}
                   </Link>
                 </li>
@@ -80,17 +95,26 @@ const Footer = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/contact-us" className="cursor-pointer text-customGray hover:text-gray-700">
+                  <Link
+                    href="/contact-us"
+                    className="cursor-pointer text-customGray hover:text-gray-700"
+                  >
                     {t("H.title1.point1")}
                   </Link>
                 </li>
                 <li>
-                  <Link href="/#services" className="text-customGray hover:text-gray-700">
+                  <Link
+                    href="/#services"
+                    className="text-customGray hover:text-gray-700"
+                  >
                     {t("H.title1.point4")}
                   </Link>
                 </li>
                 <li>
-                  <Link href="/about-us" className="text-customGray hover:text-gray-700">
+                  <Link
+                    href="/about-us"
+                    className="text-customGray hover:text-gray-700"
+                  >
                     {t("H.title1.point5")}
                   </Link>
                 </li>
@@ -98,15 +122,27 @@ const Footer = () => {
             </div>
 
             <div>
-              <h3 className="font-semibold text-black mb-4">{t("H.heading2")}</h3>
+              <h3 className="font-semibold text-black mb-4">
+                {t("H.heading2")}
+              </h3>
               <ul className="space-y-3">
                 <li>
-                  <Link scroll={true} href="/privacy" target="_blank" className="cursor-pointer text-customGray hover:text-gray-700">
+                  <Link
+                    scroll={true}
+                    href="/privacy"
+                    target="_blank"
+                    className="cursor-pointer text-customGray hover:text-gray-700"
+                  >
                     {t("H.title2")}
                   </Link>
                 </li>
                 <li>
-                  <Link scroll={true} href="/terms-conditions" target="_blank" className="cursor-pointer text-customGray hover:text-gray-700">
+                  <Link
+                    scroll={true}
+                    href="/terms-conditions"
+                    target="_blank"
+                    className="cursor-pointer text-customGray hover:text-gray-700"
+                  >
                     Terms and Conditions
                   </Link>
                 </li>
@@ -114,11 +150,17 @@ const Footer = () => {
             </div>
 
             <div>
-              <h3 className="font-semibold text-black mb-4">{t("H.heading3")}</h3>
+              <h3 className="font-semibold text-black mb-4">
+                {t("H.heading3")}
+              </h3>
               <ul className="space-y-3 text-customGray">
                 <li className="font-semibold">{t("H.title3.point1")}</li>
-                <li className="hidden md:block">{renderText(t("H.title3.point2"))}</li>
-                <li className="block md:hidden">{renderText(t("H.title3.point3"))}</li>
+                <li className="hidden md:block">
+                  {renderText(t("H.title3.point2"))}
+                </li>
+                <li className="block md:hidden">
+                  {renderText(t("H.title3.point3"))}
+                </li>
               </ul>
             </div>
           </div>
